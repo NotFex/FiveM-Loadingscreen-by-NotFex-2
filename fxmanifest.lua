9e2ad2fd8ad577b41b6da255fedbc5abdf5ba006
Felix Burwitz <62483138+NotFex@users.noreply.github.com>
2021-05-11 20:19:39 +0200

    uploaderino

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'NotFex'
+description 'LoadingScreen by NotFex.'
+version '4.2.0'
+
+files {
+    'index.html',
+    'css/style.css',
+    'js/loading.js',
+    'js/audioController.js',
+    'img/discord.svg',
+}
+
+loadscreen 'index.html'
---
+++ js/audioController.js
@@ -0,0 +1,17 @@
+var audio = document.getElementById("audioPlayer");
+audio.volume = 0.1;
+
+var audio = document.querySelector('audio');
+
+if (audio) {
+window.addEventListener('keydown', function (event) {
+    var key = event.which || event.keyCode;
+
+    if (key === 32) { // spacebar
+    // eat the spacebar, so it does not scroll the page
+    event.preventDefault();
+
+    audio.paused ? audio.play() : audio.pause();
+    }
+});
+}
